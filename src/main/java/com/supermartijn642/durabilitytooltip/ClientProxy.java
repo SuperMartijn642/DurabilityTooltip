9f386ed83618b473d2fd19b5f9f413a4d1cbc8db
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-08-03 23:00:41 +0200

    1.12 port

--- src/main/java/com/supermartijn642/durabilitytooltip/ClientProxy.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/ClientProxy.java
@@ -1,29 +1,26 @@
 package com.supermartijn642.durabilitytooltip;
 
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.util.text.*;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * Created 7/1/2021 by SuperMartijn642
  */
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy {
 
     @SubscribeEvent
     public static void onItemTooltip(ItemTooltipEvent e){
-        if(e.getItemStack().getItem().getRegistryName().getNamespace().equals("minecraft") && e.getItemStack().isDamageableItem() && !e.getFlags().isAdvanced()){
-            ITextComponent durability = new StringTextComponent(Integer.toString(e.getItemStack().getMaxDamage())).withStyle(TextFormatting.GOLD);
-            if(e.getItemStack().isDamaged()){
-                ITextComponent remainingDurability = new StringTextComponent(Integer.toString(e.getItemStack().getMaxDamage() - e.getItemStack().getDamageValue())).withStyle(TextFormatting.GOLD);
-                e.getToolTip().add(new TranslationTextComponent("durabilitytooltip.info.damaged", remainingDurability, durability).withStyle(TextFormatting.GRAY));
+        if(e.getItemStack().getItem().getRegistryName().getResourceDomain().equals("minecraft") && e.getItemStack().isItemStackDamageable() && !e.getFlags().isAdvanced()){
+            ITextComponent durability = new TextComponentString(Integer.toString(e.getItemStack().getMaxDamage())).setStyle(new Style().setColor(TextFormatting.GOLD));
+            if(e.getItemStack().isItemDamaged()){
+                ITextComponent remainingDurability = new TextComponentString(Integer.toString(e.getItemStack().getMaxDamage() - e.getItemStack().getItemDamage())).setStyle(new Style().setColor(TextFormatting.GOLD));
+                e.getToolTip().add(new TextComponentTranslation("durabilitytooltip.info.damaged", remainingDurability, durability).setStyle(new Style().setColor(TextFormatting.GRAY)).getFormattedText());
             }else
-                e.getToolTip().add(new TranslationTextComponent("durabilitytooltip.info.durability", durability).withStyle(TextFormatting.GRAY));
+                e.getToolTip().add(new TextComponentTranslation("durabilitytooltip.info.durability", durability).setStyle(new Style().setColor(TextFormatting.GRAY)).getFormattedText());
         }
     }
 }
--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -1,18 +1,17 @@
 package com.supermartijn642.durabilitytooltip;
 
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
  */
-@Mod("durabilitytooltip")
+@Mod(modid = DurabilityTooltip.MODID, name = DurabilityTooltip.NAME, version = DurabilityTooltip.VERSION, clientSideOnly = true)
 public class DurabilityTooltip {
 
+    public static final String MODID = "durabilitytooltip";
+    public static final String NAME = "Durability Tooltip";
+    public static final String VERSION = "1.0.0";
+
     public DurabilityTooltip(){
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
