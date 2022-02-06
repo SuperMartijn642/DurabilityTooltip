9371490dc4d5c03ea8470a5041d7e650e18dd126
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-02-06 01:42:10 +0100

    Use the NetworkConstants class

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -3,6 +3,7 @@ package com.supermartijn642.durabilitytooltip;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
@@ -11,7 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 public class DurabilityTooltip {
 
     public DurabilityTooltip(){
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "IGNORESERVERONLY", (a, b) -> b));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> b));
         DurabilityTooltipConfig.init();
     }
 }
