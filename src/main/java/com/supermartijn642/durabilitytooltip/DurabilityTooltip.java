ad1cab29499a4481e88add6b56653d437069833f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-02-28 01:19:36 +0100

    update version

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -10,7 +10,7 @@ public class DurabilityTooltip {
 
     public static final String MODID = "durabilitytooltip";
     public static final String NAME = "Durability Tooltip";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.9,)";
 
     public DurabilityTooltip(){
