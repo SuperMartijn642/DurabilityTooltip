aeb14ea2ddf507e7dadd1a17537ac305b4b3dbe0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-06 14:37:35 +0200

    Only initialize config if config library is present

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.durabilitytooltip;
 
 import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkConstants;
@@ -13,6 +14,7 @@ public class DurabilityTooltip {
 
     public DurabilityTooltip(){
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> b));
-        DurabilityTooltipConfig.init();
+        if(ModList.get().isLoaded("supermartijn642configlib"))
+            DurabilityTooltipConfig.init();
     }
 }
