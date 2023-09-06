d1103ac75b7353dfb113f3d1fa5c9c4e27f6a27c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-06 14:54:06 +0200

    Add blackListedMods and showWhenFull config options

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -2,11 +2,15 @@ package com.supermartijn642.durabilitytooltip;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
  */
 public class DurabilityTooltip implements ModInitializer {
+
+    public static Logger LOGGER = LoggerFactory.getLogger("durabilitytooltip");
 
     @Override
     public void onInitialize(){
--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipConfig.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipConfig.java
@@ -16,6 +16,8 @@ public class DurabilityTooltipConfig {
     public static final Supplier<TooltipColorStyle> tooltipColorStyle;
     public static final Supplier<ChatFormatting> baseTooltipColor;
     public static final Supplier<Boolean> onlyVanillaTools;
+    public static final Supplier<Boolean> showWhenFull;
+    public static final Supplier<String> blackListedMods;
 
     static{
         IConfigBuilder builder = ConfigBuilders.newTomlConfig("durabilitytooltip", null, false);
@@ -26,6 +28,8 @@ public class DurabilityTooltipConfig {
         tooltipColorStyle = builder.dontSync().comment("What colors should be used for the reactive part (numbers/text/bar characters) of the tooltip? 'base' means use the base color, 'gold' means always gold, 'varying' means green/orange/red depending on remaining durability.").define("tooltipColorStyle", TooltipColorStyle.VARYING);
         baseTooltipColor = builder.dontSync().comment("What should be the base text color of the tooltip?").define("baseTooltipColor", ChatFormatting.GRAY);
         onlyVanillaTools = builder.dontSync().comment("Should the durability tooltip only be shown on vanilla tools?").define("onlyVanillaTools", false);
+        showWhenFull = builder.dontSync().comment("Should the durability tooltip be shown when a tool is not damaged?").define("showWhenFull", true);
+        blackListedMods = builder.dontSync().comment("Which mods should be blacklisted? Items from blacklisted mods won't show the durability tooltip.").define("blackListedMods", "tconstruct,supplementaries", 0, 100);
         builder.pop();
 
         builder.build();
