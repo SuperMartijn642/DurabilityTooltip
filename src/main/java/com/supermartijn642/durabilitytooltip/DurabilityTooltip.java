c05c1b2f91e680156ac23833ccf34fdd08cfe304
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-06 14:54:06 +0200

    Add blackListedMods and showWhenFull config options

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -5,12 +5,16 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
  */
 @Mod("durabilitytooltip")
 public class DurabilityTooltip {
+
+    public static Logger LOGGER = LoggerFactory.getLogger("durabilitytooltip");
 
     public DurabilityTooltip(){
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> b));
--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
@@ -1,10 +1,16 @@
 package com.supermartijn642.durabilitytooltip;
 
+import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created 7/1/2021 by SuperMartijn642
@@ -12,9 +18,36 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DurabilityTooltipClient {
 
+    private static String lastBlackListConfig = "";
+    private static Set<String> blackListedMods = new HashSet<>();
+
+    public static boolean isBlackListed(Item item){
+        String owningMod = ForgeRegistries.ITEMS.getKey(item).getNamespace();
+
+        // Update the list of blacklisted mods
+        if(!DurabilityTooltipConfig.blackListedMods.get().equals(lastBlackListConfig)){
+            blackListedMods.clear();
+            Arrays.stream(DurabilityTooltipConfig.blackListedMods.get().split(","))
+                .map(String::trim)
+                .filter(s -> {
+                    boolean valid = s.matches("[a-z0-9_.-]+");
+                    if(!valid)
+                        DurabilityTooltip.LOGGER.error("Invalid modid '" + s + "' in config value 'blackListedMods'!");
+                    return valid;
+                })
+                .forEach(blackListedMods::add);
+            lastBlackListConfig = DurabilityTooltipConfig.blackListedMods.get();
+        }
+
+        return blackListedMods.contains(owningMod);
+    }
+
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void onItemTooltip(ItemTooltipEvent e){
-        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || e.getItemStack().getItem().getRegistryName().getNamespace().equals("minecraft")) && e.getItemStack().isDamageableItem() && (!e.getFlags().isAdvanced() || !e.getItemStack().isDamaged())){
+        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || ForgeRegistries.ITEMS.getKey(e.getItemStack().getItem()).getNamespace().equals("minecraft"))
+            && !isBlackListed(e.getItemStack().getItem())
+            && (DurabilityTooltipConfig.showWhenFull.get() || e.getItemStack().isDamaged())
+            && e.getItemStack().isDamageableItem() && (!e.getFlags().isAdvanced() || !e.getItemStack().isDamaged())){
             int maxDurability = e.getItemStack().getMaxDamage();
             int durability = maxDurability - e.getItemStack().getDamageValue();
             DurabilityTooltipConfig.tooltipStyle.get().appendTooltip(e.getToolTip(), durability, maxDurability);
