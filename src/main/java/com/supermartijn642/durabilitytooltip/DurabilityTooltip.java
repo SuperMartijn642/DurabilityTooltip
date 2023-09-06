18d97930858cbd8ce51643c4f25c186ad97f56e6
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-06 14:54:06 +0200

    Add blackListedMods and showWhenFull config options

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -6,12 +6,16 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
  */
 @Mod("durabilitytooltip")
 public class DurabilityTooltip {
+
+    public static Logger LOGGER = LogManager.getLogger("durabilitytooltip");
 
     public DurabilityTooltip(){
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
