78d1f44f923eae3f585f0dc249b1152f16d81770
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-06 14:54:06 +0200

    Add blackListedMods and showWhenFull config options

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltip.java
@@ -2,6 +2,8 @@ package com.supermartijn642.durabilitytooltip;
 
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
@@ -13,6 +15,7 @@ public class DurabilityTooltip {
     public static final String NAME = "Durability Tooltip";
     public static final String VERSION = "1.1.4";
     public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.1.6,)";
+    public static Logger LOGGER = LogManager.getLogger("durabilitytooltip");
 
     public DurabilityTooltip(){
         if(Loader.isModLoaded("supermartijn642configlib"))
--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
@@ -1,10 +1,16 @@
 package com.supermartijn642.durabilitytooltip;
 
+import net.minecraft.item.Item;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created 7/1/2021 by SuperMartijn642
@@ -12,9 +18,36 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class DurabilityTooltipClient {
 
+    private static String lastBlackListConfig = "";
+    private static Set<String> blackListedMods = new HashSet<>();
+
+    public static boolean isBlackListed(Item item){
+        String owningMod = ForgeRegistries.ITEMS.getKey(item).getResourceDomain();
+
+        // Update the list of blacklisted mods
+        if(!DurabilityTooltipConfig.blackListedMods.get().equals(lastBlackListConfig)){
+            blackListedMods.clear();
+            Arrays.stream(DurabilityTooltipConfig.blackListedMods.get().split(","))
+                .map(String::trim)
+                .filter(s -> {
+                    boolean valid = s.matches("[a-z0-9_.-]+");
+                    if(!valid)
+                        DurabilityTooltip.LOGGER.error("Invalid modid '" + s + "' in config value 'blackListedMods'!");
+                    return valid;
+                })
+                .forEach(blackListedMods::add);
+            lastBlackListConfig = DurabilityTooltipConfig.blackListedMods.get();
+        }
+
+        return blackListedMods.contains(owningMod);
+    }
+
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void onItemTooltip(ItemTooltipEvent e){
-        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || e.getItemStack().getItem().getRegistryName().getResourceDomain().equals("minecraft")) && e.getItemStack().isItemStackDamageable() && (!e.getFlags().isAdvanced() || !e.getItemStack().isItemDamaged())){
+        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || ForgeRegistries.ITEMS.getKey(e.getItemStack().getItem()).getResourceDomain().equals("minecraft"))
+            && !isBlackListed(e.getItemStack().getItem())
+            && (DurabilityTooltipConfig.showWhenFull.get() || e.getItemStack().isItemDamaged())
+            && e.getItemStack().isItemStackDamageable() && (!e.getFlags().isAdvanced() || !e.getItemStack().isItemDamaged())){
             int maxDurability = e.getItemStack().getMaxDamage();
             int durability = maxDurability - e.getItemStack().getItemDamage();
             DurabilityTooltipConfig.tooltipStyle.get().appendTooltip(e.getToolTip(), durability, maxDurability);
