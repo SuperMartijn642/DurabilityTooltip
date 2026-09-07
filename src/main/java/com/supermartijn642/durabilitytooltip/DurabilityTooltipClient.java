2626c048b58264a368be9ea365badb056fcceabb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-09-07 12:49:28 +0200

    Add handling for durability of 0

--- src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/DurabilityTooltipClient.java
@@ -1,7 +1,10 @@
 package com.supermartijn642.durabilitytooltip;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -10,6 +13,7 @@ import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -44,13 +48,17 @@ public class DurabilityTooltipClient {
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void onItemTooltip(ItemTooltipEvent e){
-        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || BuiltInRegistries.ITEM.getKey(e.getItemStack().getItem()).getNamespace().equals("minecraft"))
-            && !isBlackListed(e.getItemStack().getItem())
-            && (DurabilityTooltipConfig.showWhenFull.get() || e.getItemStack().isDamaged())
-            && e.getItemStack().isDamageableItem() && (!e.getFlags().isAdvanced() || !e.getItemStack().isDamaged())){
-            int maxDurability = e.getItemStack().getMaxDamage();
-            int durability = maxDurability - e.getItemStack().getDamageValue();
-            DurabilityTooltipConfig.tooltipStyle.get().appendTooltip(e.getToolTip(), durability, maxDurability);
+        ItemStack stack = e.getItemStack();
+        TooltipFlag flag = e.getFlags();
+        List<Component> lines = e.getToolTip();
+        if((!DurabilityTooltipConfig.onlyVanillaTools.get() || BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace().equals("minecraft"))
+            && !isBlackListed(stack.getItem())
+            && (DurabilityTooltipConfig.showWhenFull.get() || stack.isDamaged())
+            && stack.isDamageableItem() && (!flag.isAdvanced() || !stack.isDamaged())){
+            int maxDurability = stack.getMaxDamage();
+            int durability = maxDurability - stack.getDamageValue();
+            if(DurabilityTooltipConfig.showWhenBroken.get() || durability > 0)
+                DurabilityTooltipConfig.tooltipStyle.get().appendTooltip(lines, durability, maxDurability);
         }
     }
 }
