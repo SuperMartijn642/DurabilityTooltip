d85f2acad255f403ace3283b1cf519de9ac9b8ee
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-09-07 12:49:28 +0200

    Add handling for durability of 0

--- src/main/java/com/supermartijn642/durabilitytooltip/TooltipStyle.java
+++ src/main/java/com/supermartijn642/durabilitytooltip/TooltipStyle.java
@@ -49,7 +49,8 @@ public enum TooltipStyle {
                 String translationKey = durability == maxDurability ? "durabilitytooltip.info.text.full_durability"
                     : durability >= 0.4f * maxDurability ? "durabilitytooltip.info.text.damaged"
                     : durability >= 0.1f * maxDurability ? "durabilitytooltip.info.text.severely_damaged"
-                    : "durabilitytooltip.info.text.nearly_broken";
+                    : durability > 0 ? "durabilitytooltip.info.text.nearly_broken"
+                    : "durabilitytooltip.info.text.broken";
                 ITextComponent tooltip = new TranslationTextComponent(translationKey).withStyle(reactiveColor);
                 if(showTooltipHint)
                     tooltip = new TranslationTextComponent("durabilitytooltip.info.text.durability_hint", tooltip).withStyle(baseColor);
